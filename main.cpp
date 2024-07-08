7d1ca098bebdd8337c6d2834996d770aea50791a
minJeongHwan9651 <166204973+minJeongHwan9651@users.noreply.github.com>
2024-07-08 23:11:57 +0900

    first commit
    첫 업로드

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ 공부
+//
+//  Created by admin on 7/8/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Lets Start!\n";
+    return 0;
+}
